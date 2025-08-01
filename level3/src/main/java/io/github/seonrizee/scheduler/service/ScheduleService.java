2ff4befd1fede68614504120ee359c16c3efb14f
seonrizee <seonrizee@gmail.com>
2025-08-01 16:02:09 +0900

    chore(lv3): lv3 진행을 위한 lv2 모듈 복사 및 lv3 생성

---
+++ level3/src/main/java/io/github/seonrizee/scheduler/service/ScheduleService.java
@@ -0,0 +1,15 @@
+package io.github.seonrizee.scheduler.service;
+
+import io.github.seonrizee.scheduler.dto.request.ScheduleRequestDto;
+import io.github.seonrizee.scheduler.dto.response.ScheduleResponseDto;
+import io.github.seonrizee.scheduler.dto.response.SchedulesResponseDto;
+import java.util.Optional;
+
+public interface ScheduleService {
+
+    ScheduleResponseDto createSchedule(ScheduleRequestDto requestDto);
+
+    SchedulesResponseDto findAllSchedules(Optional<String> username);
+
+    ScheduleResponseDto findById(Long id);
+}
---
+++ level3/src/test/java/io/github/seonrizee/scheduler/Level3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seonrizee.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
